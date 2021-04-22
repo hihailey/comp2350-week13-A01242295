6452e7a53f2ef30438a259a62de223ca117a1765
Hailey Kim <haileykim@MacBook-Pro-3.local>
2021-04-21 20:40:26 -0700

    updated

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,18 +1,16 @@
-const MongoClient = require("mongodb").MongoClient;
-
+const database = require("mongoose");
 const is_heroku = process.env.IS_HEROKU || false;
+const databaseName = "lab_example"
 
-
-const herokuURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.kte7d.mongodb.net/lab_example?retryWrites=true&w=majority";
-
-const localURI = "mongodb://localhost/?authSource=admin&retryWrites=true;"
+const herokuURI =
+"mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/"+databas
+eName+"?retryWrites=true&w=majority"
+const localURI =
+"mongodb://localhost/"+databaseName+"?authSource=admin&retryWrites=true;"
 
 if (is_heroku) {
-	var database = new MongoClient(herokuURI, {useNewUrlParser: true, useUnifiedTopology: true});
+	database.connect(herokuURI, {useNewUrlParser: true, useUnifiedTopology: true});
+	}else {
+	database.connect(localURI, {useNewUrlParser: true, useUnifiedTopology: true});
 }
-else {
-	var database = new MongoClient(localURI, {useNewUrlParser: true, useUnifiedTopology: true});
-}
-
-module.exports = database;
 		
