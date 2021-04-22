2e495837580fe50e526431e3b006f334adaead67
Hailey Kim <haileykim@MacBook-Pro-3.local>
2021-04-21 21:03:10 -0700

    updated

--- databaseConnection.js
+++ databaseConnection.js
@@ -3,7 +3,7 @@ const is_heroku = process.env.IS_HEROKU || false;
 const databaseName = "lab_example"
 
 const herokuURI =
-"mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/"+databaseName+"?retryWrites=true&w=majority"
+"mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.kte7d.mongodb.net/"+databaseName+"?retryWrites=true&w=majority"
 const localURI =
 "mongodb://localhost/"+databaseName+"?authSource=admin&retryWrites=true;"
 
